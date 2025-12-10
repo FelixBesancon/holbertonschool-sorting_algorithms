46b0695e1dc061faabcc5bbbcd70b418c806af86
FelixBesancon <f.besancon@hotmail.fr>
2025-12-10 10:28:48 +0000

    Add 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,32 +9,31 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	int swap = 1;
-	listint_t **node = list, *tmp_node;
+	listint_t *node, *node_next, *node_prev;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	while (swap == 1)
+	node = (*list)->next;
+	while (node != NULL)
 	{
-		swap = 0;
-		node = list;
-		while ((*node)->next != NULL)
+		node_prev = node->prev;
+		node_next = node->next;
+		while (node_prev != NULL && node->n < node_prev->n)
 		{
-			if ((*node)->n > ((*node)->next)->n)
-			{
-				tmp_node = (*node)->next;
-				if ((*node)->prev != NULL)
-					((*node)->prev)->next = tmp_node;
-				if (tmp_node->next != NULL)
-					(tmp_node->next)->prev = *node;
-				(*node)->next = tmp_node->next;
-				tmp_node->prev = (*node)->prev;
-				tmp_node->next = *node;
-				(*node)->prev = tmp_node;
-				swap = 1;
-				print_list(*list);
-			}
-		*node = (*node)->next;
+			node_prev->next = node_next;
+			if (node_next != NULL)
+				node_next->prev = node_prev;
+			node_next = node_prev;
+			node_prev = node_prev->prev;
+			node->next = node_next;
+			node->prev = node_prev;
+			node_next->prev = node;
+			if (node_prev != NULL)
+				(node_prev)->next = node;
+			else
+				*list = node;
+			print_list(*list);
 		}
+		node = node->next;
 	}
 }
